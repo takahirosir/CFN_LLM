a6aa9e64217c31c586d18bc8454222beb8ed987e
takahirosir <98999068+takahirosir@users.noreply.github.com>
2023-05-24 09:24:34 +0000

    add some comments

--- KMeans.py
+++ KMeans.py
@@ -4,8 +4,13 @@ Generate a random graph using NetworkX.
 Retrieve the node positions from the graph.
 Apply the k-means algorithm to cluster the nodes based on their positions.
 Visualize the clusters.
-Here's the Python code that implements this simulation:
-
+Here's the Python code that implements this simulation:.
+要在随机图上使用k-均值算法模拟基于图的聚类，您可以按照以下步骤操作：
+使用NetworkX生成随机图。
+从图中检索节点位置。
+应用k-均值算法根据节点的位置将其聚类。
+可视化集群。
+以下是实现此模拟的Python代码:
 '''
 
 
@@ -15,11 +20,13 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 # Generate a random graph
+# 生成一个随机图
 def generate_random_graph(num_nodes, num_edges):
     G = nx.gnm_random_graph(num_nodes, num_edges)
     return G
 
 # Perform k-means clustering on node positions
+# 应用k-均值算法根据节点的位置将其聚类
 def perform_kmeans_clustering(graph, num_clusters):
     node_positions = nx.spring_layout(graph)
     positions = np.array(list(node_positions.values()))
@@ -34,9 +41,9 @@ def visualize_clusters(graph, labels):
     plt.show()
 
 # Example usage
-num_nodes = 50
-num_edges = 100
-num_clusters = 3
+num_nodes = 50    # 50个节点
+num_edges = 100     # 100条边
+num_clusters = 3  # 3类
 
 random_graph = generate_random_graph(num_nodes, num_edges)
 labels = perform_kmeans_clustering(random_graph, num_clusters)
--- README.md
+++ README.md
@@ -8,3 +8,9 @@ sudo docker build . -t cfn:v0.1
 bash start.sh
 ```
 建议使用pip install scikit-learn安装软件包" scikit-learn"，但在使用import sklearn导入的代码中。
+
+在mac本地运行Docker的时候，需要在每次build之前运行
+```shell
+cat ~/.docker/config.json
+rm  ~/.docker/config.json
+```
